4d6142dbf13d55d03e654d8d9a492cb1dedd584a
Kyle Paklaian <kyle.paklaian@DT03mKP01.local>
2018-02-05 23:51:17 -0600

    final commit

--- liri.js
+++ liri.js
@@ -7,7 +7,7 @@ var keys = require("./keys.js");
 
 
 // Load the fs package to read and write
-var fs = require("fs");
+// var fs = require("fs");
 
 // Take two arguments.
 // The first will be the action (i.e. "deposit", "withdraw", etc.)
@@ -117,5 +117,39 @@ function movie() {
     if(!movie) {
         return console.log('Movie not found!');
     }
+    
 };
 
+function doIt() {
+
+    // fs is a core Node package for reading and writing files
+    var fs = require("fs");
+
+    // This block of code will read from the "movies.txt" file.
+    // It's important to include the "utf8" parameter or the code will provide stream data (garbage)
+    // The code will store the contents of the reading inside the variable "data"
+    fs.readFile("random.txt", "utf8", function(error, data) {
+
+      // If the code experiences any errors it will log the error to the console.
+      if (error) {
+        return console.log(error);
+      }
+
+      // We will then print the contents of data
+        //console.log(data);
+
+      // Then split it by commas (to make it more readable)
+      var dataArr = data.split(",");
+
+      // We will then re-display the content as an array for later use.
+      console.log(dataArr[0]);
+      console.log(dataArr[1]);
+
+    //   cannot get the values of dataArr to run in commandline
+      var action = process.argv[2] = dataArr[0];
+      var value = process.argv[3] = dataArr[1];
+
+    });
+
+
+};
